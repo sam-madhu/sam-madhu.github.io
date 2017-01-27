a4cefec7052bbb348645fc6d9f6134e5bac60cad
Sam Madhu <94samyu@gmail.com>
2017-01-27 15:19:28 -0500

    Add files via upload

---
+++ value.js
@@ -0,0 +1,642 @@
+//template
+
+function incrementValue()
+{
+    var value = parseInt(document.getElementById('number').value, 10);
+    value = isNaN(value) ? 0 : value;
+    value++;
+    document.getElementById('number').value = value;
+    //add to total
+    document.getElementById('numbertotal').value = value*cost;
+}
+
+function decreaseValue()
+{
+    var value = parseInt(document.getElementById('number').value, 10);
+    value = isNaN(value) ? 0 : value;
+    value--;
+
+          if (value<0) {
+        value=0;
+    }
+    document.getElementById('number').value = value;
+    //decrease from total
+    document.getElementById('numbertotal').value = value*cost;
+}
+
+
+//1
+
+function incrementValue1()
+{
+    var value = parseInt(document.getElementById('number1').value, 10);
+    value = isNaN(value) ? 0 : value;
+    value++;
+    document.getElementById('number1').value = value;
+
+}
+
+function decreaseValue1()
+{
+    var value = parseInt(document.getElementById('number1').value, 10);
+    value = isNaN(value) ? 0 : value;
+    value--;
+
+      if (value<0) {
+        value=0;
+    }
+    
+    document.getElementById('number1').value = value;
+
+}
+
+//2
+
+function incrementValue2()
+{
+    var value = parseInt(document.getElementById('number2').value, 10);
+    value = isNaN(value) ? 0 : value;
+    value++;
+    document.getElementById('number2').value = value;
+}
+
+function decreaseValue2()
+{
+    var value = parseInt(document.getElementById('number2').value, 10);
+    value = isNaN(value) ? 0 : value;
+    value--;
+
+      if (value<0) {
+        value=0;
+    }
+
+    document.getElementById('number2').value = value;
+}
+
+
+//3
+
+function incrementValue3()
+{
+    var value = parseInt(document.getElementById('number3').value, 10);
+    value = isNaN(value) ? 0 : value;
+    value++;
+    document.getElementById('number3').value = value;
+}
+
+function decreaseValue3()
+{
+    var value = parseInt(document.getElementById('number3').value, 10);
+    value = isNaN(value) ? 0 : value;
+    value--;
+
+      if (value<0) {
+        value=0;
+    }
+    
+    document.getElementById('number3').value = value;
+}
+
+//4
+
+function incrementValue4()
+{
+    var value = parseInt(document.getElementById('number4').value, 10);
+    value = isNaN(value) ? 0 : value;
+    value++;
+    document.getElementById('number4').value = value;
+}
+
+function decreaseValue4()
+{
+    var value = parseInt(document.getElementById('number4').value, 10);
+    value = isNaN(value) ? 0 : value;
+    value--;
+
+      if (value<0) {
+        value=0;
+    }
+    
+    document.getElementById('number4').value = value;
+}
+
+
+//5
+
+function incrementValue5()
+{
+    var value = parseInt(document.getElementById('number5').value, 10);
+    value = isNaN(value) ? 0 : value;
+    value++;
+    document.getElementById('number5').value = value;
+}
+
+function decreaseValue5()
+{
+    var value = parseInt(document.getElementById('number5').value, 10);
+    value = isNaN(value) ? 0 : value;
+    value--;
+
+      if (value<0) {
+        value++;
+    }
+    
+    document.getElementById('number5').value = value;
+}
+
+
+//6
+
+function incrementValue6()
+{
+    var value = parseInt(document.getElementById('number6').value, 10);
+    value = isNaN(value) ? 0 : value;
+    value++;
+    document.getElementById('number6').value = value;
+}
+
+function decreaseValue6()
+{
+    var value = parseInt(document.getElementById('number6').value, 10);
+    value = isNaN(value) ? 0 : value;
+    value--;
+
+      if (value<0) {
+        value=0;
+    }
+    document.getElementById('number6').value = value;
+}
+
+//7
+
+function incrementValue7()
+{
+    var value = parseInt(document.getElementById('number7').value, 10);
+    value = isNaN(value) ? 0 : value;
+    value++;
+    document.getElementById('number7').value = value;
+}
+
+function decreaseValue7()
+{
+    var value = parseInt(document.getElementById('number7').value, 10);
+    value = isNaN(value) ? 0 : value;
+    value--;
+
+      if (value<0) {
+        value=0;
+    }
+    
+    document.getElementById('number7').value = value;
+}
+
+
+//8
+
+function incrementValue8()
+{
+    var value = parseInt(document.getElementById('number8').value, 10);
+    value = isNaN(value) ? 0 : value;
+    value++;
+    document.getElementById('number8').value = value;
+}
+
+function decreaseValue8()
+{
+    var value = parseInt(document.getElementById('number8').value, 10);
+    value = isNaN(value) ? 0 : value;
+    value--;
+
+      if (value<0) {
+        value=0;
+    }
+    
+    document.getElementById('number8').value = value;
+}
+
+//9
+
+function incrementValue9()
+{
+    var value = parseInt(document.getElementById('number9').value, 10);
+    value = isNaN(value) ? 0 : value;
+    value++;
+    document.getElementById('number9').value = value;
+}
+
+function decreaseValue9()
+{
+    var value = parseInt(document.getElementById('number9').value, 10);
+    value = isNaN(value) ? 0 : value;
+    value--;
+
+      if (value<0) {
+        value=0;
+    }
+    
+    document.getElementById('number9').value = value;
+}
+
+//10
+
+function incrementValue10()
+{
+    var value = parseInt(document.getElementById('number10').value, 10);
+    value = isNaN(value) ? 0 : value;
+    value++;
+    document.getElementById('number10').value = value;
+}
+
+function decreaseValue10()
+{
+    var value = parseInt(document.getElementById('number10').value, 10);
+    value = isNaN(value) ? 0 : value;
+    value--;
+
+      if (value<0) {
+        value=0;
+    }
+    
+    document.getElementById('number10').value = value;
+}
+
+//11
+
+function incrementValue11()
+{
+    var value = parseInt(document.getElementById('number11').value, 10);
+    value = isNaN(value) ? 0 : value;
+    value++;
+    document.getElementById('number11').value = value;
+}
+
+function decreaseValue11()
+{
+    var value = parseInt(document.getElementById('number11').value, 10);
+    value = isNaN(value) ? 0 : value;
+    value--;
+
+      if (value<0) {
+        value=0;
+    }
+    
+    document.getElementById('number11').value = value;
+}
+
+
+//12
+
+function incrementValue12()
+{
+    var value = parseInt(document.getElementById('number12').value, 10);
+    value = isNaN(value) ? 0 : value;
+    value++;
+    document.getElementById('number12').value = value;
+}
+
+function decreaseValue12()
+{
+    var value = parseInt(document.getElementById('number12').value, 10);
+    value = isNaN(value) ? 0 : value;
+    value--;
+
+      if (value<0) {
+        value=0;
+    }
+    
+    document.getElementById('number12').value = value;
+}
+
+
+//13
+
+function incrementValue13()
+{
+    var value = parseInt(document.getElementById('number13').value, 10);
+    value = isNaN(value) ? 0 : value;
+    value++;
+    document.getElementById('number13').value = value;
+}
+
+function decreaseValue13()
+{
+    var value = parseInt(document.getElementById('number13').value, 10);
+    value = isNaN(value) ? 0 : value;
+    value--;
+
+      if (value<0) {
+        value=0;
+    }
+    
+    document.getElementById('number13').value = value;
+}
+
+//14
+
+function incrementValue14()
+{
+    var value = parseInt(document.getElementById('number14').value, 10);
+    value = isNaN(value) ? 0 : value;
+    value++;
+    document.getElementById('number14').value = value;
+}
+
+function decreaseValue14()
+{
+    var value = parseInt(document.getElementById('number14').value, 10);
+    value = isNaN(value) ? 0 : value;
+    value--;
+
+      if (value<0) {
+        value=0;
+    }
+    
+    document.getElementById('number14').value = value;
+}
+
+
+//15
+
+function incrementValue15()
+{
+    var value = parseInt(document.getElementById('number15').value, 10);
+    value = isNaN(value) ? 0 : value;
+    value++;
+    document.getElementById('number15').value = value;
+}
+
+function decreaseValue15()
+{
+    var value = parseInt(document.getElementById('number15').value, 10);
+    value = isNaN(value) ? 0 : value;
+    value--;
+
+      if (value<0) {
+        value=0;
+    }
+    
+    document.getElementById('number15').value = value;
+}
+
+
+//16
+
+function incrementValue16()
+{
+    var value = parseInt(document.getElementById('number16').value, 10);
+    value = isNaN(value) ? 0 : value;
+    value++;
+    document.getElementById('number16').value = value;
+}
+
+function decreaseValue16()
+{
+    var value = parseInt(document.getElementById('number16').value, 10);
+    value = isNaN(value) ? 0 : value;
+    value--;
+
+      if (value<0) {
+        value=0;
+    }
+    
+    document.getElementById('number16').value = value;
+}
+
+//17
+
+function incrementValue17()
+{
+    var value = parseInt(document.getElementById('number17').value, 10);
+    value = isNaN(value) ? 0 : value;
+    value++;
+    document.getElementById('number17').value = value;
+}
+
+function decreaseValue17()
+{
+    var value = parseInt(document.getElementById('number17').value, 10);
+    value = isNaN(value) ? 0 : value;
+    value--;
+
+      if (value<0) {
+        value=0;
+    }
+    
+    document.getElementById('number17').value = value;
+}
+
+
+//18
+
+function incrementValue18()
+{
+    var value = parseInt(document.getElementById('number18').value, 10);
+    value = isNaN(value) ? 0 : value;
+    value++;
+    document.getElementById('number18').value = value;
+}
+
+function decreaseValue18()
+{
+    var value = parseInt(document.getElementById('number18').value, 10);
+    value = isNaN(value) ? 0 : value;
+    value--;
+
+      if (value<0) {
+        value=0;
+    }
+    
+    document.getElementById('number18').value = value;
+}
+
+//19
+
+function incrementValue19()
+{
+    var value = parseInt(document.getElementById('number19').value, 10);
+    value = isNaN(value) ? 0 : value;
+    value++;
+    document.getElementById('number19').value = value;
+}
+
+function decreaseValue19()
+{
+    var value = parseInt(document.getElementById('number19').value, 10);
+    value = isNaN(value) ? 0 : value;
+    value--;
+
+      if (value<0) {
+        value=0;
+    }
+    
+    document.getElementById('number19').value = value;
+}
+
+//20
+
+function incrementValue20()
+{
+    var value = parseInt(document.getElementById('number20').value, 10);
+    value = isNaN(value) ? 0 : value;
+    value++;
+    document.getElementById('number20').value = value;
+}
+
+function decreaseValue20()
+{
+    var value = parseInt(document.getElementById('number20').value, 10);
+    value = isNaN(value) ? 0 : value;
+    value--;
+
+      if (value<0) {
+        value=0;
+    }
+    
+    document.getElementById('number20').value = value;
+}
+
+
+
+//define costs
+
+// var cost = 10;
+var cost1 = 120;
+var cost2 = 150;
+var cost3 = 130;
+var cost4 = 700;
+var cost5 = 250;
+var cost6 = 80;
+var cost7 = 400;
+var cost8 = 90;
+var cost9 = 130;
+var cost10 = 140;
+var cost11 = 170;
+var cost12 = 240;
+var cost13 = 260;
+var cost14 = 220;
+var cost15 = 230;
+var cost16 = 300;
+var cost17 = 400;
+var cost18 = 500;
+var cost19 = 200;
+var cost20 = 120;
+
+
+
+function findTotal(){ 
+    var val1, val2, result; 
+    val1 = parseInt(document.getElementById('number1').value, 10)*cost1;
+    val2 = parseInt(document.getElementById("number2").value, 10)*cost2;
+    val3 = parseInt(document.getElementById('number3').value, 10)*cost3;
+    val4 = parseInt(document.getElementById("number4").value, 10)*cost4;
+    val5 = parseInt(document.getElementById('number5').value, 10)*cost5;
+    val6 = parseInt(document.getElementById("number6").value, 10)*cost6;
+    val7 = parseInt(document.getElementById('number7').value, 10)*cost7;
+    val8 = parseInt(document.getElementById("number8").value, 10)*cost8;
+    val9 = parseInt(document.getElementById('number9').value, 10)*cost9;
+    val10 = parseInt(document.getElementById("number10").value, 10)*cost10;
+    val11 = parseInt(document.getElementById('number11').value, 10)*cost11;
+    val12 = parseInt(document.getElementById("number12").value, 10)*cost12;
+    val13 = parseInt(document.getElementById("number13").value, 10)*cost13;
+
+    result =  (val1+val2+val3+val4+val5+val6+val7+val8+val9+val10+val11+val12+val13); 
+
+    var height = $(document).height();
+
+    var hours = document.getElementById("hours").value;
+    var mins = document.getElementById("mins").value;
+
+    var totalhours = hours * 3600000;
+    // var totalmins = mins * 60000;
+
+    // for testing
+    var totalmins = mins * 60;
+
+    var timer = totalhours + totalmins;
+
+
+
+//animate counter and hourglass
+
+    $('#numbertotal').text(result);
+
+    $('#numbertotal').prop('Counter',0).animate({
+        Counter: $('#numbertotal').text()
+    }, {
+        duration: timer,
+        easing: 'linear',
+        step: function (now) {
+            // $(this).text(Math.ceil(now));
+            $(this).text(now.toFixed(2)); 
+            $(this).text( $(this).text().replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,") );
+        }
+    });
+
+    $(".hourglass").animate({ 
+    'padding-top' : height - 10,
+    'backgroundColor' : "#ff0000",
+  }, timer);
+
+
+    $('.stop').click(function() {
+    $('#numbertotal').stop();
+    $('.hourglass').stop();
+    });
+
+// if you didnt enter any attendees or time, resort to default timer screen
+
+    if (result == 0) {
+    $('.hourglass').stop();
+ }
+
+    if (timer == 0) {
+    $('.hourglass').hide();
+    $('#numbertotal').text(0);
+ }
+
+
+}
+
+
+
+var hours =0;
+var mins =0;
+var seconds =0;
+
+$('.start').click(function(){
+      startTimer();
+});
+
+$('.stop').click(function(){
+      clearTimeout(timex);
+});
+
+$('#reset').click(function(){
+      hours =0;      mins =0;      seconds =0;
+  $('#hours','#mins').html('00:');
+  $('#seconds').html('00');
+});
+
+function startTimer(){
+  timex = setTimeout(function(){
+      seconds++;
+    if(seconds >59){seconds=0;mins++;
+       if(mins>59) {
+       mins=0;hours++;
+         if(hours <10) {$("#hours").text('0'+hours+':')} else $("#hours").text(hours+':');
+                       }
+                       
+    if(mins<10){                     
+      $("#mins").text('0'+mins+':');}       
+       else $("#mins").text(mins+':');
+                   }    
+    if(seconds <10) {
+      $("#seconds").text('0'+seconds);} else {
+      $("#seconds").text(seconds);
+      }
+     
+    
+      startTimer();
+  },1000);
+}
+    
